d2d1f86a6f05380e17ab637837b775698887c305
Alex Ghiculescu <alex@tanda.co>
2021-03-28 15:00:18 -0500

    add update action

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -6,4 +6,10 @@ class BasketsController < ApplicationController
   def edit
     @basket = Basket.first
   end
+
+  def update
+    @basket = Basket.first
+    @basket.save
+    redirect_to edit_basket_path
+  end
 end
